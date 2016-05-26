a20450a761ea6faad1e14bbaa607c7eedc7b4df3
Hermann Krumrey <hermann@krumreyh.com>
2016-05-26 15:53:03 +0200

    Defined the GenericWindow abstract class

--- src/main/java/net/namibsun/jfworks/interfaces/GenericWindow.java
+++ src/main/java/net/namibsun/jfworks/interfaces/GenericWindow.java
@@ -1,7 +1,70 @@
+/*
+Copyright 2016 Hermann Krumrey
+
+This file is part of jfworks.
+
+    jfworks is a collection of classes and methods that are designed to make
+    cross-framework GUI design easier by standardizing a lot of framework-specific
+    functionality with a unified interface.
+
+    jfworks is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    jfworks is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with jfworks. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package main.java.net.namibsun.jfworks.interfaces;
 
+
 /**
- * Created by hermann on 5/26/16.
+ * Abstract Class that defines how a window is initialized, run and closed
+ * It offers a basic Constructor, and three methods to control the GUI control
+ * flow.
  */
-public class GenericWindow {
+abstract class GenericWindow {
+
+    /**
+     * Constructor that defines a standardized method signature and calls the layOut()
+     * method.
+     * @param title - The Title of the GUI Window
+     * @param parent - The parent of the GUI Window
+     * @param hideParent - If set to True, the parent window will be hidden
+     */
+    GenericWindow(String title, GenericWindow parent, boolean hideParent) {
+        this.layOut();
+    }
+
+    /**
+     * Handles the layout of the GUI Window
+     */
+    public abstract void layOut();
+
+    /**
+     * Starts the GUI
+     */
+    public abstract void start();
+
+    /**
+     * Forces the Gui to stop
+     */
+    public abstract void stop();
+
 }
+
+
+// Order:
+// WidgetGenerator extends Framework implement WidgetGenerator
+// -> Dialogshower
+// => Threading
+// -> Value Getter
+// Setter
+// Clearer
+// __> Positioner
